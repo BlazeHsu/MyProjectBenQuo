3b9a68999d13e05ad6c5a7161931ce7ecec29b40
Blazelisa <blazelisa@gmail.com>
2018-05-26 20:21:11 +0800

    RecycleView

--- app/src/main/java/tw/com/blazelisa/myprojectbenquo/GiudeActivity.java
+++ app/src/main/java/tw/com/blazelisa/myprojectbenquo/GiudeActivity.java
@@ -3,20 +3,79 @@ package tw.com.blazelisa.myprojectbenquo;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tw.com.blazelisa.myprojectbenquo.adapter.RecycleAdapter;
+import tw.com.blazelisa.myprojectbenquo.bean.Glid;
+import tw.com.blazelisa.myprojectbenquo.config.Config;
+
 public class GiudeActivity extends AppCompatActivity {
     private TextView gide_tv ;
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RecycleAdapter recycleAdapter;
+    private List<Glid> list = new ArrayList<>();
+    private String url = Config._URL+"getinfo.php";
+    RecyclerView.LayoutManager manager;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giude);
-        gide_tv = findViewById(R.id.giude_tv);
-        gide_tv.setText("GuideActivity");
+//        gide_tv = findViewById(R.id.giude_tv);
+//        gide_tv.setText("GuideActivity");
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recycle_view_giude);
+        manager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);
+        requestQueue = Volley.newRequestQueue(this.getApplicationContext());
+
+
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                int count = 0;
+                while(count < response.length()){
+                    try {
+                        JSONObject object = response.getJSONObject(count);
+                        list.add(new Glid(object.getString("id"),object.getString("title")));
+                        count++;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                recycleAdapter = new RecycleAdapter(list,GiudeActivity.this);
+                recyclerView.setAdapter(recycleAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
     }
 }
--- app/src/main/java/tw/com/blazelisa/myprojectbenquo/adapter/RecycleAdapter.java
+++ app/src/main/java/tw/com/blazelisa/myprojectbenquo/adapter/RecycleAdapter.java
@@ -1,33 +1,63 @@
 package tw.com.blazelisa.myprojectbenquo.adapter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tw.com.blazelisa.myprojectbenquo.R;
+import tw.com.blazelisa.myprojectbenquo.bean.Glid;
+import tw.com.blazelisa.myprojectbenquo.config.Config;
 
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHolder>{
+    List<Glid> list = new ArrayList<>();
+    Activity activity;//like Context
 
+    public RecycleAdapter(List<Glid> list, Context context) {
+        this.list = list;
+        this.activity = (Activity) context;//this.context = context
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+//        頁面關聯
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_items,parent,false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+//        元件內容傳送
+        holder.items_title.setText(list.get(position).getTitle());
+        String path = Config._URL+"images/"+list.get(position).getId()+".png";
+        Glide.with(activity).load(path).into(holder.items_image);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-
+        ImageView items_image;
+        TextView items_title;
         public MyViewHolder(View itemView) {
             super(itemView);
+        items_image = itemView.findViewById(R.id.items_image);
+        items_title = itemView.findViewById(R.id.items_title);
+
         }
     }
 }

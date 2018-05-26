11b8a38dec528c114d12c5e30a9c377498e28e9d
Blazelisa <blazelisa@gmail.com>
2018-05-26 14:59:46 +0800

    家RecycleViewAdapter

--- app/src/main/java/tw/com/blazelisa/myprojectbenquo/bean/Glid.java
+++ app/src/main/java/tw/com/blazelisa/myprojectbenquo/bean/Glid.java
@@ -1,4 +1,30 @@
 package tw.com.blazelisa.myprojectbenquo.bean;
 
 public class Glid {
+    private String id;
+    private String title;
+
+    public Glid() {
+    }
+
+    public Glid(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
